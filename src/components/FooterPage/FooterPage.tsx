c54b05b2589ce36019a194d4478348cd042b9887
Israel Ramos <arnulfo_israel@hotmail.com>
2024-07-13 23:38:41 -0600

    feat(footer): update footer

--- src/components/FooterPage/FooterPage.tsx
+++ src/components/FooterPage/FooterPage.tsx
@@ -9,19 +9,14 @@ import {
 
 const customFooterTheme: DeepPartial<FlowbiteFooterTheme> = {
   root: {
-    base: "mx-auto container border-solid border-2 w-full bg-white dark:bg-gray-800 md:flex md:items-center md:justify-between",
+    base: "mx-auto container border-solid border-2 w-full bg-white dark:bg-gray-800 md:flex md:items-center md:justify-center",
   },
 };
 
 const FooterPage = () => (
   <Footer container theme={customFooterTheme}>
-    <FooterCopyright
-      href="https://flowbite-react.com/"
-      by="Flowbite™"
-      year={2024}
-    />
     <FooterLinkGroup>
-      <FooterLink href="#">Published with {"name"}</FooterLink>
+      <FooterLink href="https://pages.github.com/">Published with Gihub Pages</FooterLink>
     </FooterLinkGroup>
   </Footer>
 );
